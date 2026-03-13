72bebcf0ab072a781835f06453c104d834edbcf3
John Jones <johnjones@Johns-MacBook-Pro-2.local>
2026-03-13 12:25:00 -0700

    Initial commit

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by John Jones on 3/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
